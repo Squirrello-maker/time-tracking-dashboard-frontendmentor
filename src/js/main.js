cb884343400d03835d53eda1ccae8bcc49b72cbb
Emil Olejnik <olejnike2000@gmail.com>
2021-12-22 13:18:07 +0100

    Completed project

---
+++ src/js/main.js
@@ -0,0 +1,73 @@
+class ActivityHandler
+{
+    constructor()
+    {
+        this.hours = document.querySelectorAll('.card__content__hours-box');
+        this.previousTime = document.querySelectorAll('.card__content__previous-stat');
+    }
+    dailyActivity()
+    {
+        fetch('http://localhost:3000/src/JSON/data.json')
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data[1]);
+            this.hours.forEach((element, index) => {
+                this.hours[index].textContent = `${data[index].timeframes.daily.current}hrs`;
+                this.previousTime[index].textContent = `Last Daily - ${data[index].timeframes.daily.previous}hrs`;
+            });
+        });
+    }
+    weeklyActivity()
+    {
+        fetch('http://localhost:3000/src/JSON/data.json')
+        .then(res => res.json())
+        .then(data =>{
+            this.hours.forEach((element, index) => {
+                this.hours[index].textContent = `${data[index].timeframes.weekly.current}hrs`;
+                this.previousTime[index].textContent = `Last Weekly - ${data[index].timeframes.weekly.previous}hrs`;
+            });
+        });
+    }
+    monthlyActivity()
+    {
+        fetch('http://localhost:3000/src/JSON/data.json')
+        .then(res => res.json())
+        .then(data =>{
+            this.hours.forEach((element, index) => {
+                this.hours[index].textContent = `${data[index].timeframes.monthly.current}hrs`;
+                this.previousTime[index].textContent = `Last Monthly - ${data[index].timeframes.monthly.previous}hrs`;
+            });
+        });
+    }
+    addFnsToArr()
+    {
+        this.funsArr = [this.dailyActivity, this.weeklyActivity, this.monthlyActivity];
+    }
+}
+class DOMHandler
+{
+    static getBtns()
+    {
+        const sortingButtons = document.querySelectorAll('.report-button');
+        return sortingButtons;
+    }
+    static addEventListeners(btns, funs, obj)
+    {
+        btns.forEach((btn, i) =>{
+            btn.addEventListener('click', funs[i].bind(obj));
+        });
+
+    }
+}
+
+class App
+{
+    static init()
+    {
+       const activity = new ActivityHandler();
+       activity.addFnsToArr();
+       DOMHandler.addEventListeners(DOMHandler.getBtns(), activity.funsArr, activity);
+
+    }
+}
+App.init();
